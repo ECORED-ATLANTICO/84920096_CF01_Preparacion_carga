08458a98bab06fb2b2f051bd2d629f796bd0f7c4
devcontenidosinteractivos <98177573+AndresDiazPinto@users.noreply.github.com>
2025-08-01 18:14:55 -0500

    Empaquetado

--- src/config/global.js
+++ src/config/global.js
@@ -170,7 +170,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/dist.pdf',
+        download: 'downloads/CFA1_84920096_DU.pdf',
       },
       {
         icono: 'fas fa-download',
